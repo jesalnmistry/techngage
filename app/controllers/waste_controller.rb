9eef5d914b2c558e2205fcf9ae256bec0c2df80b
Jesal Mistry <jesalnmistry@gmail.com>
2016-04-26 00:36:42 +0530

    adding logos for type of waste

--- app/controllers/waste_controller.rb
+++ app/controllers/waste_controller.rb
@@ -3,8 +3,16 @@ class WasteController < ApplicationController
 	WASTES = { "dry waste" => ["cardboards", "nails", "wires", "rubber", "newspaper", "cloth", "plastic bags", "aluminium foils", "cans", "wrappers", "broken utensils", "boxes", "tubes", "tires", "medical packages"],
 			"wet waste" => ["bones", "kitchen waste", "meat", "fruits", "coconut peel", "eggs", "fish", "finger nails", "tea leaves", "vegetable peels", "hair"],
 			"garden waste" => ["dry leaves", "flowers"],
-			"e-waste" => ["bulb", "batteries", "toys", "computer", "mobiles", "pen drives", "televisions", "monitor", "VCRs", "telephones", "fax machines", "stereos", "tablet computer"], "hazardous waste" => ["vehicle batteries", "solvents", "waste inks", "lighters", "pesticides", "vehicle fluids", "paints", "lead", "mercury", "copper", "zinc"] 
-		}
+			"e-waste" => ["bulb", "batteries", "toys", "computer", "mobiles", "pen drives", "televisions", "monitor", "VCRs", "telephones", "fax machines", "stereos", "tablet computer"],
+			"hazardous waste" => ["vehicle batteries", "solvents", "waste inks", "lighters", "pesticides", "vehicle fluids", "paints", "lead", "mercury", "copper", "zinc"] 
+	}
+
+	WASTE_LOGOS = { "dry waste" => "dry_waste",
+				"wet waste" => "wet_waste",
+				"garden waste" => "garden_waste",
+				"e-waste" => "e_waste",
+				"hazardous waste" => "hazardous_waste"
+	}
 
 	def show
 		@waste_query = params[:waste_query]
@@ -15,5 +23,6 @@ class WasteController < ApplicationController
 				end
 			end
 		end
+		@waste_logo = WASTE_LOGOS[@type_of_waste]
 	end
 end
