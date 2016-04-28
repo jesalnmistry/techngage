b9111120ca4327d5d7c864a7eb044e8c33723a61
Jesal Mistry <jesalnmistry@gmail.com>
2016-04-28 17:06:56 +0530

    1> added description along with waste type on search result page ; 2>changed case of waste types ; 3> adjusting css schedule button so that it doesn't break.

--- app/controllers/waste_controller.rb
+++ app/controllers/waste_controller.rb
@@ -1,31 +1,31 @@
 class WasteController < ApplicationController
 
 	WASTES = { 
-		"dry waste" => ["cardboards", "nails", "wires", "rubber", "newspaper", "cloth", "plastic bags", "aluminium foils", "cans", "wrappers", "broken utensils", "boxes", "tubes", "tires", "medical packages"],
-		"wet waste" => ["bones", "kitchen waste", "meat", "fruits", "coconut peel", "eggs", "fish", "finger nails", "tea leaves", "vegetable peels", "hair"],
-		"garden waste" => ["dry leaves", "flowers", "grass", "hedge trimmings"],
-		"e-waste" => ["bulb", "batteries", "toys", "computer", "mobiles", "pen drives", "televisions", "monitor", "VCRs", "telephones", "fax machines", "stereos", "tablet computer"],
-		"hazardous waste" => ["vehicle batteries", "solvents", "waste inks", "lighters", "pesticides", "vehicle fluids", "paints", "lead", "mercury", "copper", "zinc"] 
+		"Dry Waste" => ["cardboards", "nails", "wires", "rubber", "newspaper", "cloth", "plastic bags", "aluminium foils", "cans", "wrappers", "broken utensils", "boxes", "tubes", "tires", "medical packages"],
+		"Wet Waste" => ["bones", "kitchen waste", "meat", "fruits", "coconut peel", "eggs", "fish", "finger nails", "tea leaves", "vegetable peels", "hair"],
+		"Garden Waste" => ["dry leaves", "flowers", "grass", "hedge trimmings"],
+		"E-Waste" => ["bulb", "batteries", "toys", "computer", "mobiles", "pen drives", "televisions", "monitor", "VCRs", "telephones", "fax machines", "stereos", "tablet computer"],
+		"Hazardous Waste" => ["vehicle batteries", "solvents", "waste inks", "lighters", "pesticides", "vehicle fluids", "paints", "lead", "mercury", "copper", "zinc"] 
 	}
 
 	WASTE_LOGOS = { 
-		"dry waste" => "dry_waste",
-		"wet waste" => "wet_waste",
-		"garden waste" => "garden_waste",
-		"e-waste" => "e_waste",
-		"hazardous waste" => "hazardous_waste"
+		"Dry Waste" => "dry_waste",
+		"Wet Waste" => "wet_waste",
+		"Garden Waste" => "garden_waste",
+		"E-Waste" => "e_waste",
+		"Hazardous Waste" => "hazardous_waste"
 	}
 
 	WASTE_DESCRIPTION = {
-		"dry waste" => "Dry waste includes all items that are not considered wet or soiled items. Anything that can be kept for an extended period without decomposing.",
-		"wet waste" => "Wet waste includes all organic, non-dry waste. It usually include kitchen waste.",
-		"garden waste" => "Green waste is biodegradable waste that can be composed of garden or park waste, such as grass or flower cuttings and hedge trimmings.",
-		"e-waste" => "E-waste is any waste created by discarded electronic devices and components as well as substances involved in their manufacture or use.",
-		"hazardous waste" => "Hazardous waste is waste that poses substantial or potential threats to public health or the environment."
+		"Dry Waste" => "Dry waste includes all items that are not considered wet or soiled items. Anything that can be kept for an extended period without decomposing.",
+		"Wet Waste" => "Wet waste includes all organic, non-dry waste. It usually includes kitchen waste.",
+		"Garden Waste" => "Garden waste is biodegradable waste that can be composed of garden or park waste, such as grass or flower cuttings and hedge trimmings.",
+		"E-Waste" => "E-waste is any waste created by discarded electronic devices and components as well as substances involved in their manufacture or use.",
+		"Hazardous Waste" => "Hazardous waste is waste that poses substantial or potential threats to public health or the environment."
 	}
 
 	def show
-		@title = "Throw away your waste"
+		@title = "Waste type search result"
 		@waste_query = params[:waste_query]
 		WASTES.each do |key , value|
 			value.each do |waste|
@@ -35,5 +35,6 @@ class WasteController < ApplicationController
 			end
 		end
 		@waste_logo = WASTE_LOGOS[@type_of_waste]
+		@waste_description = WASTE_DESCRIPTION[@type_of_waste]
 	end
 end
